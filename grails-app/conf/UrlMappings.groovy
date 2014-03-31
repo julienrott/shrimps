7dd1838e8e6d42180f326bafba968f302917c21a
Julien ROTT <julien.rott@gmail.com>
2014-03-31 16:08:42 +0200

    correction dans urlmapping pour console plugin

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -11,6 +11,7 @@ class UrlMappings {
         "500"(view:'/error')
 
         "/infos/$id"(controller: "pageInfo", action: "show")
+        "/console"(controller: "console")
         "/$categorieProduit"(controller: "produit", action: "list")
 	}
 }
